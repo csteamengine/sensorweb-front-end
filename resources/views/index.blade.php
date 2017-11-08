823d138ccca6a9adf68048d0c0a8a386da6a838e
Gregory Steenhagen <gregorysteenhagen@Gregorys-MacBook-Pro.local>
2017-11-07 20:36:30 -0600

    added leaf image

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,7 @@
         <div class="col-md-8 col-md-offset-2" align="center">
             <h1>Welcome to SensorWeb</h1>
             <p>The newest, coolest doohickey to hit the market since buttered parsnips.</p>
+            <img src="/images/leaf.png" class="img img-responsive" style="height: 400px; opacity: .5;">
         </div>
     </div>
 </div>
